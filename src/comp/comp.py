34c2e5c690b14872264196c04473368977457475
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-02-14 12:46:47 -0800

    fixed mistake in array composition challenge

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -61,7 +61,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [Human(ele.name.upper(), ele.age) for ele in humans]
+g = [Human(ele.name.upper(), ele.age + 5) for ele in humans]
 
 for h in g:
     print(h.name, h.age)
